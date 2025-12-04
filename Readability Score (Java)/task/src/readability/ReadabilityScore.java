d03d2b6ca56e8023caf4c8f384f550eeb7211551
Paweł Daniluk <pawel.daniluk@kotalbert.net>
2025-12-04 10:02:06 +0100

    Clear stage 2

--- Readability Score (Java)/task/src/readability/ReadabilityScore.java
+++ Readability Score (Java)/task/src/readability/ReadabilityScore.java
@@ -2,16 +2,28 @@ package readability;
 
 public class ReadabilityScore {
     private final String text;
+    private final String[] sentences;
 
     public ReadabilityScore(String text) {
         this.text = text;
+        this.sentences = text.split("[.!?]+");
+    }
 
+    private int countWords(String sentence) {
+        String[] words = sentence.split("\\s+");
+        return words.length;
     }
 
     public Score getScore() {
-        if (text.length() <= 100)
+        int totalWords = 0;
+        for (String sentence : sentences) {
+            totalWords += countWords(sentence);
+        }
+        double averageWordsPerSentence = (double) totalWords / sentences.length;
+        if (Math.floor(averageWordsPerSentence) <= 10) {
             return Score.EASY;
-        else
+        } else {
             return Score.HARD;
+        }
     }
 }
